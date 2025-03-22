3af25c45b862ead14e7ab4759bc402bcf5d3c5fa
Jarin tasnim <163668265+jarintasnim2n1@users.noreply.github.com>
2025-03-22 23:16:23 +0600

    Add files via upload
    updated code

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='isolate'>
+    <div >
         <div className='absolute inset-0 -z-10'>
             <div className='absolute top-0 -left-1/4 w-1/2 h-1/2 bg-amber-400 dark:bg-amber-500 rounded-full blur-3xl opacity-20 '>      
             </div>
